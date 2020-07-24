73b783ec21d699ac0f9cdb0a937bd466bd2dbe76
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-07-24 12:12:41 -0400

    added cuboid class

--- challenges/classes.js
+++ challenges/classes.js
@@ -12,3 +12,27 @@
 //     this.width = width
 //     this.height = height
 //   }
+
+// CuboidMaker.prototype.volume = function(){
+//     return this.length * this.width * this.height
+
+// CuboidMaker.prototype.surfaceArea = function() {
+//     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+//   }
+
+class CuboidMaker{
+    constructor(length, width, height){
+        this.length = length
+        this.width = width
+        this.height = height
+    }
+    volume(){
+        return this.length * this.width * this.height
+    }
+    surfaceArea(){
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+    }
+}
+const cuboid = new CuboidMaker(4, 5, 5)
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
